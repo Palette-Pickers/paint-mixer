85c54699127c085c2a57ba1d866b425326bd4881
Ben Beekman <bbeekman@gmail.com>
2023-08-01 20:40:19 -0700

    defined default palette colors and format

--- src/Mixer.tsx
+++ src/Mixer.tsx
@@ -1,29 +1,53 @@
-import React from 'react';
-import {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import mixbox from 'mixbox';
 
 import './style.css';
 
 export default function Mixer () {
-    const [currentColor, setCurrentColor] = useState('rgb(0, 33, 255)')
+    const [currentColor, setCurrentColor] = useState('rgb(0, 33, 255)');
     const [palette, setPalette] = useState([
-        ['Cadmium Yellow', '#feec00', 0],
-        ['Hansa Yellow', '#fcd300', 0],
-        ['Cadmium Orange', '#ff6900', 0],
-        ['Cadmium Red', '#ff2702', 0],
-        ['Quinacridone Magenta', '#80022e', 0],
-        ['Cobalt Violet', '#4e0042', 0],
-        ['Ultramarine Blue', '#190059', 0],
-        ['Cobalt Blue', '#002185', 0],
-        ['Phthalo Blue', '#0d1b44', 0],
-        ['Phthalo Green', '#003c32', 0],
-        ['Permanent Green', '#076d16', 0],
-        ['Sap Green', '#6b9404', 0],
-        ['Burnt Sienna', '#7b4800', 0],
-        ['Burnt Umber', '#4e2a04', 0]])
+        [{ label: 'Cadmium Yellow', color: 'rgb(254, 236, 0)' }],
+        [{ label: 'Hansa Yellow', color: 'rgb(252, 211, 0)' }],
+        [{ label: 'Cadmium Orange', color: 'rgb(255, 105, 0)' }],
+        [{ label: 'Cadmium Red', color: 'rgb(255, 39, 2)' }],
+        [{ label: 'Quinacridone Magenta', color: 'rgb(78, 0, 66)' }],
+        [{ label: 'Cobalt Violet', color: 'rgb(150, 0, 255)'}],
+        [{ label: 'Ultramarine Blue', color: 'rgb(25, 0, 89)' }],
+        [{ label: 'Cerulean Blue', color: 'rgb(0, 33, 133)' }],
+        [{ label: 'Phthalo Blue', color: 'rgb(13, 27, 68)' }],
+        [{ label: 'Phthalo Green', color: 'rgb(0, 60, 50)' }],
+        [{ label: 'Permanent Green', color: 'rgb(7, 109, 22)' }],
+        [{ label: 'Sap Green', color: 'rgb(107, 148, 4)' }],
+        [{ label: 'Burnt Sienna', color: 'rgb(123, 72, 0)' }],
+    ])
+
+    useEffect(() => {
+        setPalette([
+            [{ label: 'Cadmium Yellow', color: 'rgb(254, 236, 0)'}],
+            [{ label: 'Hansa Yellow', color: 'rgb(252, 211, 0)' }],
+            [{ label: 'Cadmium Orange', color: 'rgb(255, 105, 0)' }],
+            [{ label: 'Cadmium Red', color: 'rgb(255, 39, 2)' }],
+            [{ label: 'Quinacridone Magenta', color: 'rgb(78, 0, 66)' }],
+            [{ label: 'Cobalt Violet', color: 'rgb(150, 0, 255)'}],
+            [{ label: 'Ultramarine Blue', color: 'rgb(25, 0, 89)' }],
+            [{ label: 'Cerulean Blue', color: 'rgb(0, 33, 133)' }],
+            [{ label: 'Phthalo Blue', color: 'rgb(13, 27, 68)' }],
+            [{ label: 'Phthalo Green', color: 'rgb(0, 60, 50)' }],
+            [{ label: 'Permanent Green', color: 'rgb(7, 109, 22)' }],
+            [{ label: 'Sap Green', color: 'rgb(107, 148, 4)' }],
+            [{ label: 'Burnt Sienna', color: 'rgb(123, 72, 0)' }]
+        ]);
+        setCurrentColor('rgb(255, 255, 255)');
+    }, [palette]);
+
     return (
     <div className='mixer'>
             <div style={{backgroundColor: currentColor}} className='color-box'></div>
-            
+            <div className='palette'>
+
+
+            </div>
+
     </div>
     );
 }
