fd03200718cd340765e5a9788378d4910d2d793b
Ben Beekman <bbeekman@gmail.com>
2023-08-09 18:36:58 -0700

    added partsInMix values to palette and UI

--- src/Mixer.tsx
+++ src/Mixer.tsx
@@ -4,29 +4,31 @@ import mixbox from 'mixbox';
 import './style.css';
 
 export default function Mixer () {
-    const [currentColor, setCurrentColor] = useState('rgb(0, 33, 255)');
+    const [currentColor, setCurrentColor] = useState('rgb(0, 0, 0)');
     const [mixingRatio, setMixingRatio] = useState(0.5);
-    const [palette, setPalette] = useState([
-        { "label": "Cadmium Yellow", "color": "rgb(254, 236, 0)" },
-        { "label": "Hansa Yellow", "color": "rgb(252, 211, 0)" },
-        { "label": "Cadmium Orange", "color": "rgb(255, 105, 0)" },
-        { "label": "Cadmium Red", "color": "rgb(255, 39, 2)" },
-        { "label": "Quinacridone Magenta", "color": "rgb(78, 0, 66)" },
-        { "label": "Cobalt Violet", "color": "rgb(150, 0, 255)" },
-        { "label": "Ultramarine Blue", "color": "rgb(25, 0, 89)" },
-        { "label": "Cerulean Blue", "color": "rgb(0, 33, 133)" },
-        { "label": "Phthalo Blue", "color": "rgb(13, 27, 68)" },
-        { "label": "Phthalo Green", "color": "rgb(0, 60, 50)" },
-        { "label": "Permanent Green", "color": "rgb(7, 109, 22)" },
-        { "label": "Sap Green", "color": "rgb(107, 148, 4)" },
-        { "label": "Burnt Sienna", "color": "rgb(123, 72, 0)" }
-    ])
+    const paletteColors = [
+        {"label": "Cadmium Yellow", "color": "rgb(254, 236, 0)", "partsInMix": 1},
+        {"label": "Hansa Yellow", "color": "rgb(252, 211, 0)", "partsInMix": 0},
+        {"label": "Cadmium Orange", "color": "rgb(255, 105, 0)", "partsInMix": 0},
+        {"label": "Cadmium Red", "color": "rgb(255, 39, 2)", "partsInMix": 0},
+        {"label": "Quinacridone Magenta", "color": "rgb(78, 0, 66)", "partsInMix": 0},
+        {"label": "Cobalt Violet", "color": "rgb(150, 0, 255)", "partsInMix": 0},
+        {"label": "Ultramarine Blue", "color": "rgb(25, 0, 89)", "partsInMix": 0},
+        {"label": "Cerulean Blue", "color": "rgb(0, 33, 133)", "partsInMix": 1},
+        {"label": "Phthalo Blue", "color": "rgb(13, 27, 68)", "partsInMix": 0},
+        {"label": "Phthalo Green", "color": "rgb(0, 60, 50)", "partsInMix": 0},
+        {"label": "Permanent Green", "color": "rgb(7, 109, 22)", "partsInMix": 0},
+        {"label": "Sap Green", "color": "rgb(107, 148, 4)", "partsInMix": 0},
+        {"label": "Burnt Sienna", "color": "rgb(123, 72, 0)", "partsInMix": 0}
+    ];
+    const [palette, setPalette] = useState(paletteColors);
 
     const makeColorSwatches = () => {
         if (palette.length) {
             return palette.map((swatch, i) => {
                 return (
-                    <div key={i} className="swatch" style={{ backgroundColor: `${swatch.color}`}}>
+                    <div key={i} className="swatch" style={{backgroundColor: `${swatch.color}`}}>
+                        {swatch.partsInMix}
                     </div>
                 )
             })
@@ -38,10 +40,19 @@ export default function Mixer () {
         console.log('Current color + first swatch makes '+mixed);
     }
 
+    const mixColorFromPalette = (palette) => {
+        let totalParts = 0;
+        for (let i = 0; i < palette.length; i++) {
+            totalParts += palette[i].partsInMix;
+        }
+        console.log('Total parts of paint used is' + totalParts);
+        
+    }
+
     let paletteSwatches = makeColorSwatches();
 
-    const addToPalette = () => {
-        const newPalette = mixbox(currentColor, 13);
+    const addToPalette = (color) => {
+        const newPalette = mixbox(color, 13);
         setPalette(newPalette);
     }
 
