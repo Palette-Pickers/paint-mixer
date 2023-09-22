c5f3ba4cdb5c2a0bcdba86707df73e0776f21383
Ben Beekman <bbeekman@gmail.com>
2023-09-21 21:38:23 -0700

    improved UI of color picker component

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -3,8 +3,10 @@ import styles from './ColorPicker.module.scss';
 import Wheel from "@uiw/react-color-wheel";
 import ShadeSlider from '@uiw/react-color-shade-slider';
 import EditableInputRGBA from '@uiw/react-color-editable-input-rgba';
+import EditableInput from '@uiw/react-color-editable-input';
 import tinycolor from "tinycolor2";
 import {AiOutlineClose} from 'react-icons/ai';
+import {hsvaToRgba, hsvaToHex} from '@uiw/color-convert';
 
 
 interface ColorPickerProps {
@@ -16,19 +18,48 @@ interface ColorPickerProps {
 
 const ColorPicker: React.FC<ColorPickerProps> = ({ color, onChange, onClose, onConfirm }) => {
     return (
-        <div className={styles.ColorPicker} style={{ background: tinycolor(color).toRgbString() }}>
-            <button className={styles.closeButton} onClick={onClose}>
+        <div className={styles.ColorPicker}
+            style={{background: tinycolor(color).toRgbString()}}
+        >
+            <button className={styles.closeButton}
+                style={{
+                    color: tinycolor(hsvaToRgba(color)).isDark() ? 'white' : 'black',
+                    transition: 'color 0.1s ease-in-out'
+                }}
+                onClick={onClose}
+            >
                 <AiOutlineClose />
             </button>
-            <Wheel color={color} onChange={(newColor) => onChange({ ...color, ...newColor.hsva })} />
+            <Wheel color={color}
+                onChange={(newColor) => onChange({...color, ...newColor.hsva})}
+            />
             <div className={styles.shadeSlider}>
                 <ShadeSlider
                     hsva={color}
-                    onChange={(newShade) => onChange({ ...color, ...newShade })}
+                    onChange={(newShade) => onChange({...color, ...newShade})}
                 />
             </div>
-            <EditableInputRGBA hsva={color} placement="top" onChange={(newColor) => onChange({ ...color, ...newColor.hsva })} />
-            <button onClick={onConfirm}>Add</button>
+            <EditableInputRGBA
+                hsva={color}
+                placement="top"
+                onChange={(newColor) => onChange({...color, ...newColor.hsva})}
+            />
+            <div style={{ padding: '0.5rem' }}>
+                <EditableInput
+                    label="Hex"
+                    value={hsvaToHex(color)}
+                    placement={'left'}
+                    style={{ width: '4.5rem', alignItems: 'space-between'}}
+                />
+            </div>
+            <button
+                onClick={onConfirm}
+                style={{
+                    backgroundColor: tinycolor(hsvaToRgba(color)).isDark() ? '#FFF4' : '#0004',
+                    color: tinycolor(hsvaToRgba(color)).isDark() ? 'black' : 'white',
+                    transition: 'all 0.2s ease-in-out'
+                }}
+            >OK</button>
         </div>
     );
 }
